d0a7873b3434ea42abf6df1e3db9fdf7930de7c0
Alex Onate <alexonate@gmail.com>
2014-04-08 12:34:24 -0400

    creating folders

---
+++ Onate_Alex_0410_Expressions_Personal/js/script.js
@@ -0,0 +1,10 @@
+//Expressoins - Arithmetic Operations
+
+
+//area of the triangle is half of the width times the height
+var width = 8;
+var height = 7;
+var area = width * height/2;
+console.log(area);
+
+
---
+++ Onate_Alex_0410_Expressions_Wacky/js/script.js
@@ -0,0 +1,9 @@
+// JavaScript
+
+var x=5+5;
+var y="5"+5;
+var z="Hello"+5;
+
+console.log(x);
+console.log(y);
+console.log(z);
